00b0eb5bc07201bc5267badac998c8dacbaa0852
FamaLJ <blongjohn2017@kellogg.northwestern.edu>
2016-06-09 11:52:20 -0500

    Setup attendee look up reference sheet

--- app/controllers/nets_controller.rb
+++ app/controllers/nets_controller.rb
@@ -1,6 +1,7 @@
 class NetsController < ApplicationController
   def index
     @enrollments = Enrollment.all
+    @enrollmentsa = Enrollment.all
     @users = User.all
     @breakoutsessions = Breakoutsession.all
     #  @enrollments = current_user.enrollments
